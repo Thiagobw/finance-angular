448b9719cd50b2ba0c9411a4ab647b8af5e4959e
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2025-01-25 12:09:18 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  //{ path:'', pathMatch:'full', loadChildren: () => import('./home/home.route').then(r => r.HOME_ROUTES) },
+
+];
